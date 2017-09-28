1f52960548682d33468152e21e8f09f5e9cf20ad
nickveil <31191433+nickveil@users.noreply.github.com>
2017-09-28 16:12:39 -0400

    MVP complete

--- app/main.js
+++ app/main.js
@@ -1,43 +1,71 @@
-//Globals
+// Globals
 var restartButton = document.getElementById('restartButton');
 var spaces = document.getElementsByClassName('space');
-var symbols = ['O','X'];
+var symbols = ['O', 'X'];
 var turn = 0;
+var notification = document.getElementById('notification');
+var winnerMessage = document.getElementById('winnerMessage');
 
-
-// Wait for page to load before doing something
-document.onreadystatechange = function(){
-	if (document.readyState == "interactive"){
-		restartButton.onclick = startGame;
-		startGame();
-	}
+// Waits for page to load before doing interesting things
+document.onreadystatechange = function() {
+  if (document.readyState == "interactive") {
+    restartButton.onclick = startGame;
+    startGame();
+  }
 };
 
-function startGame(){
+function startGame() {
 
-	
+  // Reset turn counter
+  turn = 0;
 
+  // Remove winner notification and make sure div is hidden
+  winnerMessage.innerHTML = '';
+  notification.style.display = 'none';
 
-	// reset turn counter
-	turn = 0;
-
-	// remove winner notification and make sure div is hidden
-
-	//clear board and add click events on squares
-
-	for (i=0;i<spaces.length;i++){
-		spaces[i].innerHTML="";
-		spaces[i].addEventListener("click", takeSpace);
-	}
-
-
-
+  // Clear board and add click events on squares
+  for (var i = 0; i < spaces.length; i++) {
+    spaces[i].innerHTML = '';
+    spaces[i].addEventListener("click", takeSpace);
+  }  
 
 }
 
 function takeSpace() {
 
-	turn++;
-	this.innerHTML = symbols[turn % 2];
-	this.removeEventListener("click",takeSpace);
+  turn++;
+  var currentPlayer = symbols[turn % 2];
+  this.innerHTML = currentPlayer;
+  this.removeEventListener("click", takeSpace);  
+
+  for (var i = 0; i < wins.length; i++) {
+    if (checkForWin(wins[i])) {
+
+      // No more clicking!
+      for (var j = 0; j < spaces.length; j++) {
+        spaces[j].removeEventListener("click", takeSpace);
+      }
+      // Notify the players
+      notification.style.display = 'block';
+      winnerMessage.innerHTML = "Yay! " + currentPlayer + " won!";
+    }
+    else {
+      if (turn == 9) {
+        if(notification.style.display != "block"){
+        notification.style.display = "block";
+        winnerMessage.innerHTML = "Bummer! It's a draw!";
+        }
+      }
+    }
+  }
+
 }
+
+function checkForWin(winArray) {
+    
+  return spaces[winArray[0]].innerHTML !== '' && 
+    spaces[winArray[0]].innerHTML === spaces[winArray[1]].innerHTML && 
+    spaces[winArray[0]].innerHTML === spaces[winArray[2]].innerHTML;
+  
+}
+
